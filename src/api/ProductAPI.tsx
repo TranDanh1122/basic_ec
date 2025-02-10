ec207ea90cef712b70f483935a016e5b4c016805
TranDanh1122 <trandanh14042000@gmail.com>
2025-02-10 22:34:16 +0700

    product list search + filter

--- src/api/ProductAPI.tsx
+++ src/api/ProductAPI.tsx
@@ -3,8 +3,12 @@ export const ProductAPI = {
     categories: (limit?: number) => api.get(`/products/categories${limit ? `?limit=${limit}` : ""}`),
     products: (filter?: Filter) => {
         const query = new URLSearchParams()
+        let url = `/products`
         if (filter) {
-            if (filter.search) query.append("q", filter.search)
+            if (filter.search) {
+                query.append("q", filter.search)
+                url = `${url}/search`
+            }
             if (filter.sort) {
                 query.append("sortBy", "title")
                 query.append("order", filter.sort)
@@ -14,7 +18,9 @@ export const ProductAPI = {
                 query.append("skip", filter.skip.toString())
                 query.append("limit", "4")
             }
+
         }
-        return api.get(`/products?${query.toString()}`)
+
+        return api.get(`${url}?${query.toString()}`)
     }
 }
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,9 +1,11 @@
 import { ProductAPI } from "@/api/ProductAPI";
 import ProductItem from "@/components/Home/ProductItem";
 import BreadcrumbCPN from "@/components/Shared/BreadcrumbCPN";
-import Loading from "@/components/Shared/Loading";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useDebound } from "@/hooks/useDebound";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { SortAsc, SortDesc } from "lucide-react";
 import React from "react";
 const defaultFilter: Filter = {
     search: "",
@@ -11,12 +13,12 @@ const defaultFilter: Filter = {
     skip: 0,
 }
 export default function ProductList(): React.JSX.Element {
-    const [filter, setFilter] = React.useState<Filter>(defaultFilter)
-    // const [data, setData] = React.useState<Product[]>([])
+    const [filter, setFilter] = React.useState<Filter>(defaultFilter);
+    const debound = React.useRef<number>(null)
+
     const { data: queryData, fetchNextPage, isFetchingNextPage, hasNextPage, isLoading } = useInfiniteQuery({
         queryKey: ["products", filter],
         queryFn: async ({ pageParam }) => {
-            console.log(pageParam); // cái này luôn bằng 0
 
             const response = await ProductAPI.products({ ...filter, skip: pageParam })
             return response.data
@@ -27,22 +29,51 @@ export default function ProductList(): React.JSX.Element {
         },
         initialPageParam: 0,
     })
-    if (isLoading) return <div className="w-10 h-10 border-2 animate-spin mx-auto my-20"></div>
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (debound.current) clearTimeout(debound.current)
+        debound.current = setTimeout(() => {
+            setFilter(prev => ({ ...prev, search: e.target.value }));
+        }, 300);
+    }
+    const data = React.useMemo(() => {
+        return queryData?.pages.flatMap(page => page.products)
+    }, [queryData])
+    React.useEffect(() => {
+        return () => {
+            if (debound.current) clearTimeout(debound.current);
+        };
+    }, []);
     return <div>
         <BreadcrumbCPN items={[{ name: "Home", link: "/" }, { name: "Products", link: "/products" }]} />
-
-        <div className="flex items-center justify-center gap-4 flex-wrap gap-y-6">
-            {queryData &&
-                queryData.pages.flatMap(page => page.products).map((el: Product) => <div key={el.id} className="w-[calc(25%-12px)]">
-                    <ProductItem product={el}></ProductItem>
-                </div>)
-            }
-            {hasNextPage && <Button variant="outline" className="flex mx-auto cursor-pointer mt-8"
-                onClick={() => fetchNextPage()}>
-                {isFetchingNextPage ? <div className="w-5 h-5 border-2 animate-spin"></div> : "Load more"}
-            </Button>}
-
+        <div className="flex items-center justify-between my-8">
+            <Input className="w-4/5" placeholder="Search some thing..." onChange={handleChange} />
+            <div  onClick={() => setFilter((fil) => ({ ...fil, sort: filter.sort == "asc" ? "desc" : "asc" }))}>
+                {filter.sort == "asc" && <SortAsc />}
+                {filter.sort == "desc" && <SortDesc />}
+            </div>
         </div>
+        {isLoading && <div className="w-10 h-10 border-2 animate-spin mx-auto my-20"></div>}
+        {
+            !isLoading && <div className="flex items-center justify-center gap-4 flex-wrap gap-y-6">
+                {data &&
+                    data.map((el: Product) => <div key={el.id} className="w-[calc(25%-12px)]">
+                        <ProductItem product={el}></ProductItem>
+                    </div>)
+                }
+                {
+                    (!data || data.length == 0) && <p className="font-bold">Not found</p>
+                }
+                {hasNextPage && <div>
+                    <Button variant="outline" className="flex mx-auto cursor-pointer mt-8"
+                        onClick={() => fetchNextPage()}>
+                        {isFetchingNextPage ? <div className="w-5 h-5 border-2 animate-spin"></div> : "Load more"}
+                    </Button>
+                </div>}
+
+            </div>
+
+        }
+
 
     </div>
 }
