def8d0dc8ed207a6953398e37f6e1c44f65e0777
TranDanh1122 <trandanh14042000@gmail.com>
2025-02-11 13:48:02 +0700

    add speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import { store } from './redux/store.ts'
 import { HashRouter } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from './components/ui/toaster.tsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <HashRouter>
         <App />
+        <SpeedInsights/>
         <Toaster />
       </HashRouter>
     </Provider>
